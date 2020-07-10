3e554ce86bb0fb978bf565dfa6fa17cc007eca43
serfend <serfend@foxmail.com>
2020-07-10 08:44:08 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Configuration.AutoStratManager;
+using IpSwitch.Helper;
+using Microsoft.Win32.TaskScheduler;
+using NETworkManager.Settings;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Project.Core.Protector
+{
+	internal static class Program
+	{
+		/// <summary>
+		///  The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			var startManager = new FunctionBySchedule();
+			var regStartManager = new FunctionByReg();
+			startManager.EnableAsync();
+			regStartManager.EnableAsync();
+			Application.Run(new Main());
+		}
+	}
+}
