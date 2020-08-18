f29d0dd0c6841b4bbd89eeb23ca5a02fab73b9b0
serfend <serfend@foxmail.com>
2020-08-18 08:15:39 +0800

    refactor[res]description

--- PingProtector.BLL/Network/PingDetector/PingDetector.cs
+++ PingProtector.BLL/Network/PingDetector/PingDetector.cs
@@ -1,23 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Project.Core.Protector.BLL.Network.PingDetector
 {
-	public class PingDetector : IDisposable
+	public class PingDetector
 	{
 		private readonly PingOptions options;
 		private readonly byte[] buffer = Encoding.ASCII.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 
 		/// <summary>
-		/// instance of <see cref="System.Net.NetworkInformation.Ping"/>
-		/// </summary>
-		public Ping Ping { get; set; }
-
-		/// <summary>
 		/// host to ping
 		/// </summary>
-		public string Host { get; set; }
+		public string[] Host { get; set; }
+
+		public bool Enabled { get => timer.Enabled; set => timer.Enabled = value; }
 
 		/// <summary>
 		/// if set , it would raise event while ping success
@@ -35,9 +37,8 @@ namespace Project.Core.Protector.BLL.Network.PingDetector
 
 		public event EventHandler<PingSuccessEventArgs> OnPingReply;
 
-		public PingDetector(PingOptions options = null, string host = null)
+		public PingDetector(PingOptions options = null, string[] host = null)
 		{
-			Ping = new Ping();
 			if (options == null) options = new PingOptions
 			{
 				// Use the default Ttl value which is 128,
@@ -60,17 +61,36 @@ namespace Project.Core.Protector.BLL.Network.PingDetector
 
 		public System.Timers.Timer timer;
 
-		public PingReply Check(string host = null, int timeout = 3000)
+		public IEnumerable<PingReply> Check(string[] hosts = null, int timeout = 3000)
 		{
-			if (host == null) host = Host;
-			var result = Ping.Send(host, timeout, buffer, options);
-			if (result.Status == IPStatus.Success) OnPingReply?.Invoke(this, new PingSuccessEventArgs(result));
-			return result;
-		}
-
-		public void Dispose()
-		{
-			((IDisposable)Ping).Dispose();
+			if (hosts == null) hosts = Host;
+			Debug.WriteLine($"check ping:{hosts.Length}");
+			var r = new List<Task<PingReply>>();
+			foreach (var host in hosts)
+			{
+				var waiter = new AutoResetEvent(false);
+				var task = new Task<PingReply>(() =>
+				{
+					using (var Ping = new Ping())
+					{
+						PingReply reply = null;
+						try
+						{
+							reply = Ping.Send(host, timeout, buffer, options);
+						}
+						catch (Exception)
+						{
+						}
+						if (reply?.Status == IPStatus.Success) OnPingReply?.Invoke(this, new PingSuccessEventArgs(reply));
+						return reply;
+					}
+				});
+				task.Start();
+				r.Add(task);
+			}
+			var tasks = r.ToArray();
+			Task.WaitAll(tasks);
+			return tasks.Select(t => t.Result);
 		}
 	}
 }
